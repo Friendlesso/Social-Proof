9e33df09bd652d355225ceed744fc1abdc7a166c
Mihailo <mihailoless@gmail.com>
2025-12-24 19:26:01 +0100

    Add z-index to background divs in App component
    Updated background divs to include z-index for proper layering.

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Page />
-      <div className="bg-contain bg-no-repeat fixed bg-[url('./assets/images/bg-pattern-top-desktop.svg')] h-175 w-175 top-0 left-0"></div>
-      <div className="bg-contain bg-no-repeat fixed bg-[url('./assets/images/bg-pattern-bottom-desktop.svg')] h-175 w-175 bottom-0 right-0"></div>
+      <div className="bg-contain bg-no-repeat fixed bg-[url('./assets/images/bg-pattern-top-desktop.svg')] h-175 w-175 top-0 left-0 -z-10"></div>
+      <div className="bg-contain bg-no-repeat fixed bg-[url('./assets/images/bg-pattern-bottom-desktop.svg')] h-175 w-175 bottom-0 right-0 -z-10"></div>
     </>
   )
 }
