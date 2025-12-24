315ded3f9a859b8431ae153e9d0004bf07452c9d
Mihailo <mihailoless@gmail.com>
2025-12-24 19:26:33 +0100

    Add 'relative z-10' class to Page section

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -5,7 +5,7 @@ import { Reviews } from "../Reviews/Reviews";
 export function Page() {
   
   return (
-    <section className="max-w-[90vw] w-full flex flex-col md:gap-20 gap-3">
+    <section className="max-w-[90vw] w-full flex flex-col md:gap-20 gap-3 relative z-10">
       <div className="flex md:flex-row flex-col justify-around md:gap-0 gap-6">
         <Description />
         <Reviews />
